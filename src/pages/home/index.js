0b97992e6675e58a6b074fece62bb0150dc3a441
zjf <zhangjianfei.com>
2019-11-06 17:33:06 +0800

    react create

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import './index.less'
+class Home extends PureComponent {
+    render(){
+        return(
+            <div className='home-contanier'>
+                <div className='title-contanier'>hello world</div>
+            </div>
+        )
+    }
+}
+export default Home
